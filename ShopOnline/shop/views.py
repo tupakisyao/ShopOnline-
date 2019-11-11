ced0c06560e43d6361232ebddb7da8b9cf7c65c5
Doszhan132 <56818626+Doszhan132@users.noreply.github.com>
2019-11-11 14:01:58 +0600

    addcart

--- ShopOnline/shop/views.py
+++ ShopOnline/shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
-
+from cart.forms import CartAddProductForm
 def ProductList(request, category_slug=None):
     category = None
     categories = Category.objects.all()
@@ -17,7 +17,7 @@ def ProductList(request, category_slug=None):
 
 def ProductDetail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-
+    cart_product_form = CartAddProductForm()
     return render(request, 'shop/product/detail.html',
                              {'product': product,
-                              })
+                              'cart_product_form': cart_product_form})
